ad1b4927dfff664ac3e688426ef173d04ab0a73b
Emmanuel-Olabanji <timtim2k10@gmail.com>
2022-10-26 21:51:57 +0100

    Revert "Merge branch 'emmanuel'"
    This reverts commit 4a7dbf10767225533512c311daae00695dbb3935, reversing
    changes made to a323b923742b2f133f6adcf7f80a345bb418bd6b.

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerShooting : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject bullets;
+    [SerializeField]
+    private Transform SightTransform;
+    [SerializeField]
+    private Transform allBullets;
+    [SerializeField]
+    private float bulletDistance = 20f;
+
+    private PlayerInput playerInput;
+    private Transform cameraTransform;
+    private InputAction shootAction;
+
+    // Start is called before the first frame update
+    void Awake() {
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+        shootAction = playerInput.actions["Shoot"];
+    }
+
+    private void OnEnable() {
+        shootAction.performed += _ => ShootGun();
+    }
+
+    private void OnDisable() {
+        shootAction.performed -= _ => ShootGun();
+    }
+
+    private void ShootGun() {
+        RaycastHit bulletHit;
+        GameObject bullet = GameObject.Instantiate(bullets, SightTransform.position, Quaternion.identity, allBullets);
+        BulletController bulletController = bullet.GetComponent<BulletController>();
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out bulletHit, Mathf.Infinity)) {
+            bulletController.target = bulletHit.point;
+            bulletController.bulletHit = true;
+        }
+        else {
+            bulletController.target = cameraTransform.position + cameraTransform.forward * bulletDistance;
+            bulletController.bulletHit = true;
+        }
+        shootAction.performed -= _ => ShootGun();
+    }
+
+    // Start is called before the first frame update
+    //private void Start() { 
+    
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
