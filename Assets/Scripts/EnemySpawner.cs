6f20489d47c19389d220e7b99416263126aff4e8
Emmanuel-Olabanji <timtim2k10@gmail.com>
2022-11-06 23:54:05 +0000

    prefabs

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private float respawnTime = 5f;
+    private float nextSpawnTime;
+    [SerializeField]
+    private int maxEnemy;
+
+
+  
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GameObject[] enemyCount = GameObject.FindGameObjectsWithTag("Enemy");
+
+        int thingyCount = enemyCount.Length;
+
+
+
+        if (thingyCount <= maxEnemy){
+            if (Time.time >= nextSpawnTime)
+            {
+                nextSpawnTime = Time.time + respawnTime;
+
+                GameObject.Instantiate(enemyPrefab, transform.position, transform.rotation);
+            }
+        }
+
+
+
+    }
+       
+}
