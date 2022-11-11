140cb02ea35a0541c3db0eeb7cadab08792d061c
QasimB2001 <QasimB2001@hotmail.co.uk>
2022-11-11 09:10:50 +0000

    Made some bug fixes and commented code
    Made some bug fixes, commented code and cleaned up some files.

--- Assets/Scripts/Player Scripts/Powerup.cs
+++ Assets/Scripts/Player Scripts/Powerup.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
+    //initialises GameObject variables
     public GameObject SpeedPowerup;
     public GameObject HealthPowerup;
     public GameObject DamagePowerup;
     Coroutine speedCoroutine;
     Coroutine damageCoroutine;
 
+    //Spawns a random powerup
     public void createPowerup() {
+        //Has a 25% random chance of spawning a powerup on enemy death
         float smallSpawnChance = Random.Range(1, 5);
         if (smallSpawnChance == 1) {
             Vector3 spawnPosition = transform.position;
             spawnPosition.y -= 0.75f;
+            
+            //Randomly picks one of the three powerups to spawn
             float randomPick = Random.Range(1, 4);
             if (randomPick == 1) {
                 Instantiate(SpeedPowerup, spawnPosition, Quaternion.identity);
@@ -28,17 +33,21 @@ public class Powerup : MonoBehaviour
         }
     }
 
+    //Checks if the player has picked up the powerup and then applies the affects and removes the powerup object
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
+            //checks if player picked up speed-powerup and applies affect, then disables and transports the powerup away from view
             if (gameObject.name.Contains("SpeedPowerup")) {
                 speedPowerup();
                 gameObject.GetComponent<Renderer>().enabled = false;
                 transform.position = new Vector3(0, -100, 0);
             }
+            //checks if player picked up health-powerup and applies affect, then disables and transports the powerup away from view
             else if (gameObject.name.Contains("HealthPowerup")) {
                 healthPowerup();
                 Destroy(gameObject);
             }
+            //checks if player picked up damage-powerup and applies affect, then disables and transports the powerup away from view
             else if (gameObject.name.Contains("DamagePowerup")) {
                 damagePowerup();
                 gameObject.GetComponent<Renderer>().enabled = false;
@@ -47,24 +56,31 @@ public class Powerup : MonoBehaviour
         }
     }
 
+    //applies the speed power up affect to player by doubling their walking speed
     private void speedPowerup() {
         GameObject player = GameObject.Find("Player");
         player.GetComponent<PlayerMovement3>().walkSpeed = 10f;
 
+        //Uses Coroutine to see if player is already under the speed power up affect, if so resets the timer
         if (speedCoroutine != null) {
             StopCoroutine(speedCoroutine);
             speedCoroutine = null;
         }
+        //Uses Coroutine to apply affect for 10 seconds, then returns to normal
         speedCoroutine = StartCoroutine(normalSpeed());
     }
 
+    //Uses Coroutine to apply speed affect for 10 seconds, then returns to normal
     private IEnumerator normalSpeed() {
         yield return new WaitForSeconds(10);
         GameObject player = GameObject.Find("Player");
         player.GetComponent<PlayerMovement3>().walkSpeed = 5f;
+
+        //destroys the speed powerup as its no longer needed
         Destroy(gameObject);
     }
 
+    //applies the health powerup by increasing health by 20
     private void healthPowerup() {
         if (playerHealth.PlayerHP < 80) {
             playerHealth.PlayerHP += 20;
@@ -74,24 +90,30 @@ public class Powerup : MonoBehaviour
         }
     }
 
+    //applies the damage power up affect to player by doubling their gun's bullet damage
     public void damagePowerup() {
         GameObject player = GameObject.Find("Player");
         GameObject bullet = player.GetComponent<PlayerShooting>().bullets;
         bullet.GetComponent<BulletController>().bulletDamage = 20;
 
+        //Uses Coroutine to see if player is already under the damage power up affect, if so resets the timer
         if (damageCoroutine != null)
         {
             StopCoroutine(damageCoroutine);
             damageCoroutine = null;
         }
+        //Uses Coroutine to apply affect for 10 seconds, then returns to normal
         damageCoroutine = StartCoroutine(normalDamage());
     }
 
+    //Uses Coroutine to apply damagec affect for 10 seconds, then returns to normal
     private IEnumerator normalDamage() {
         yield return new WaitForSeconds(10);
         GameObject player = GameObject.Find("Player");
         GameObject bullet = player.GetComponent<PlayerShooting>().bullets;
         bullet.GetComponent<BulletController>().bulletDamage = 10;
+
+        //destroys the damage powerup as its no longer needed
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/Player Scripts/playerHealth.cs
+++ Assets/Scripts/Player Scripts/playerHealth.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 
 public class playerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //initialises player health property variables
     public static float PlayerMaxHP = 100f;
     public static float PlayerHP = 100f;
     public TextMeshProUGUI PlayerHPText;
     public static bool GameOver;
+
+    // Start is called before the first frame update
     void Start()
     {
         GameOver = false;
         PlayerHP = PlayerMaxHP;
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -28,6 +31,7 @@ public class playerHealth : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
+
     // basic method that takes as a paraeter a float value and substracts it from current health
     public void TakeFallDamage(float damage)
     {
@@ -35,7 +39,7 @@ public class playerHealth : MonoBehaviour
         CheckHealth();
     }
 
-    //if the player health is 0 sets gameover to true
+    // if the player health is 0 sets gameover to true
     private void CheckHealth()
     {
         if (PlayerHP <= 0)
@@ -45,6 +49,7 @@ public class playerHealth : MonoBehaviour
         }
     }
 
+    // Reduces player health by the damage amount, if player health is less than 0 then ends the game
     public void TakeDamage(int damageAmount)
     {
         if (PlayerHP - damageAmount <= 0)
