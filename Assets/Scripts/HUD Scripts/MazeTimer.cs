dd41b7cf5022ce6e21977341a29f17ccedadafff
QasimB2001 <QasimB2001@hotmail.co.uk>
2023-01-08 21:15:14 +0000

    Added timer to maze level and added speed powerup and bullet to level 1
    Added the timer to maze level and added speed powerup and bullet to level 1

---
+++ Assets/Scripts/HUD Scripts/MazeTimer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeTimer : MonoBehaviour
+{
+    public float mazeTimer = 300;
+    public Text timerDisplayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Reduces timer each frame
+        if (mazeTimer > 0) {
+            mazeTimer -= Time.deltaTime;
+        }
+        //Sets timer to 0 if it goes below the 0 mark
+        else {
+            mazeTimer = 0;
+        }
+
+        SetTimerDisplayText(mazeTimer);
+    }
+
+    //Updates the timer text with the current remaining time
+    void SetTimerDisplayText(float remainingTime)
+    {
+        //Sets display timer to 0 if it goes below the 0 mark
+        if (remainingTime < 0)
+        {
+            remainingTime = 0;
+        }
+
+        //Changes float to int
+        int minutes = (int)remainingTime / 60;
+        int seconds = (int)remainingTime % 60;
+
+        string strMinutes = "00";
+        string strSeconds = "00";
+
+        //formats int minutes to string in clock style format
+        if (minutes < 10) {
+            strMinutes = "0" + minutes.ToString();
+        }
+        else {
+            strMinutes = minutes.ToString();
+        }
+
+        //formats int seconds to string in clock style format
+        if (seconds < 10)
+        {
+            strSeconds = "0" + seconds.ToString();
+        }
+        else
+        {
+            strSeconds = seconds.ToString();
+        }
+
+        //Changes text to show updated minutes and seconds
+        timerDisplayText.text = strMinutes + ":" + strSeconds;
+
+    }
+}
